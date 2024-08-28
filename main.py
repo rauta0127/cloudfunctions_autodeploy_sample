5dae5b32cefae2ba755ef2e6311486089999da8e
rauta0127 <takumaurata0127@gmail.com>
2024-08-28 16:50:12 +0900

    edit deploy yaml

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import functions_framework
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
-def subscribe(cloud_event: CloudEvent) -> None:
+def main(cloud_event: CloudEvent) -> None:
     # Print out the data from Pub/Sub, to prove that it worked
     print(
         "Hello, " + base64.b64decode(cloud_event.data["message"]["data"]).decode() + "!"
